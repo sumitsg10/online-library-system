430704f7d1ad347e1ac42f22bfecf832653162b6
Shubham Gaur <25231246+sumitsg10@users.noreply.github.com>
2018-07-23 02:53:57 +0530

    Add files via upload

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2017 Online Library Management System |<a href="https://sumitsg10.me/" target="_blank"  > Designed by : Shubham Gaur</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
